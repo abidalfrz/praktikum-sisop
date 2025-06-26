6f5ec82eaaa839d18621f77e4b33e68033ce6bff
abidalfrz <abidalfaridzi18@gmail.com>
2025-06-26 14:22:27 +0700

    Edited shell.c

--- FP/src/shell.c
+++ FP/src/shell.c
@@ -74,15 +74,135 @@ void parseCommand(char* buf, char* cmd, char arg[2][64]) {
 
 // TODO: 6. Implement cd function
 void cd(byte* cwd, char* dirname) {
+  struct node_fs nodeT;
+  int i;
+  readSector(&nodeT, FS_NODE_SECTOR_NUMBER);
+
+  if(strcmp(dirname, "/")){
+    *cwd = FS_NODE_P_ROOT;
+
+  }else if(strcmp(dirname, "..")){
+    if(*cwd == FS_NODE_P_ROOT){
+      return;
+    }
+
+    *cwd = nodeT.nodes[*cwd].parent_index;
+
+  }else{
+    for(i = 0; i < FS_MAX_NODE; i++){
+      if(strcmp(nodeT.nodes[i].node_name, dirname) && 
+      nodeT.nodes[i].parent_index == *cwd && 
+      nodeT.nodes[i].data_index == FS_NODE_D_DIR){
+        *cwd = i;
+        return;
+      }
   
+    }
+  }
+
 
 }
 
 // TODO: 7. Implement ls function
-void ls(byte cwd, char* dirname) {}
+void ls(byte cwd, char* dirname) {
+  struct node_fs nodeT;
+  int i;
+  byte get = cwd;
+  readSector(&nodeT, FS_NODE_SECTOR_NUMBER);
+
+  if(strcmp(dirname, ".") || strcmp(dirname, "")){
+    for(i = 0; i < FS_MAX_NODE; i++){
+      if(nodeT.nodes[i].parent_index == cwd){
+        printString(nodeT.nodes[i].node_name);
+        printString("\n");
+      }
+    }
+
+  }else{
+    for(i = 0; i < FS_MAX_NODE; i++){
+      if(strcmp(nodeT.nodes[i].node_name, dirname) && 
+      nodeT.nodes[i].parent_index == cwd && 
+      nodeT.nodes[i].data_index == FS_NODE_D_DIR){
+        get = i;
+        break;
+      }
+    }
+  
+    if(get == cwd) return;
+  
+    for(i = 0; i < FS_MAX_NODE; i++){
+      if(nodeT.nodes[i].parent_index == get){
+        printString(nodeT.nodes[i].node_name);
+        printString("\n");
+      }
+    }
+  }
+
+}
 
 // TODO: 8. Implement mv function
-void mv(byte cwd, char* src, char* dst) {}
+void mv(byte cwd, char* src, char* dst) {
+  struct node_fs nodeT;
+  int i, j = 0, idx = -1;
+  byte get;
+  readSector(&nodeT, FS_NODE_SECTOR_NUMBER);
+
+  // find /
+  while(dst[j] != '\0'){
+    if(dst[j] == '/'){
+      break;
+    }
+    j++;
+  }
+
+  for(i = 0; i < FS_MAX_NODE; i++){
+    if(strcmp(nodeT.nodes[i].node_name, src) &&
+    nodeT.nodes[i].parent_index == cwd &&
+    nodeT.nodes[i].data_index != FS_NODE_D_DIR){
+      idx = i;
+      break;
+    }
+  }
+
+  if(idx == -1) return;
+
+  if(j == 0){
+    nodeT.nodes[idx].parent_index = FS_NODE_P_ROOT;
+    strcpy(nodeT.nodes[idx].node_name, dst + j + 1);
+    writeSector(&nodeT, FS_NODE_SECTOR_NUMBER);
+
+  }else if(j == 2){
+    get = nodeT.nodes[cwd].parent_index;
+
+    nodeT.nodes[idx].parent_index = get;
+    strcpy(nodeT.nodes[idx].node_name, dst + j + 1);
+    writeSector(&nodeT, FS_NODE_SECTOR_NUMBER);
+
+  }else{
+    get = cwd;
+    char name[64];
+    memcpy(name, dst, j);
+    name[j] = '\0';
+
+
+    for(i = 0; i < FS_MAX_NODE; i++){
+      if(strcmp(nodeT.nodes[i].node_name, name) &&
+      nodeT.nodes[i].parent_index == cwd &&
+      nodeT.nodes[i].data_index == FS_NODE_D_DIR){
+        get = i;
+        break;
+      }
+    }
+
+    if(get == cwd) return;
+
+    nodeT.nodes[idx].parent_index = get;
+    strcpy(nodeT.nodes[idx].node_name, dst + j + 1);
+    writeSector(&nodeT, FS_NODE_SECTOR_NUMBER);
+  
+  }
+
+}
 
 // TODO: 9. Implement cp function
 void cp(byte cwd, char* src, char* dst) {}
